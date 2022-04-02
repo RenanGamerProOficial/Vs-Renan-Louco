f494b06564243f1b7271a210f6da6d3cdd6646b9
RenanGamerProOficial <69207411+RenanGamerProOficial@users.noreply.github.com>
2022-04-02 20:10:32 -0300

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -81,9 +81,10 @@ class CreditsState extends MusicBeatState
 		#end
 
 		var pisspoop:Array<Array<String>> = [ //Name - Icon name - Description - Link - BG Color
-		        ['Android Port'],
-		        ['Saw (M.A. Jigsaw)','saw','Main Coder','https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ', 'F73838'],
+		        ['Vs Renan Louco'],
+		        ['Renan','renan','Main Coder','https://www.youtube.com/channel/RenanGamerProOficialJogos', 'F73838'],
 		        [''],
+                        [''],
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',							'https://twitter.com/Shadow_Mario_',	'444444'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',						'https://twitter.com/RiverOaken',		'C30085'],
